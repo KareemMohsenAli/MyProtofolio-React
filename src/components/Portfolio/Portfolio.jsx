d4dce96b5e03eb1de73e86bd1f524d6590928206
hp <kareemtall99@gmail.com>
2023-08-25 20:07:35 +0300

    some modification

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,18 +2,55 @@ import React, { useContext } from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css";
-import Sidebar from "../../img/sidebar.png";
-import Ecommerce from "../../img/ecommerce.png";
-import HOC from "../../img/hoc.png";
-import MusicApp from "../../img/musicapp.png";
+import youtube from "../../img/youtube.png";
+import socail from "../../img/social.png";
+import movie from "../../img/movie.png";
+import food from "../../img/food.png";
+import chart from "../../img/chart.png";
+import upwork from "../../img/upwork.png";
 import { themeContext } from "../../Context";
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div className="portfolio" id="portfolio">
+   
+    <>
+     <div className="portfolio" id="portfolio">
       {/* heading */}
       <span style={{color: darkMode?'white': ''}}>Recent Projects</span>
+      <span>Front-End</span>
+
+      {/* slider */}
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        grabCursor={true}
+        className="portfolio-slider"
+      >
+        <SwiperSlide>
+          <a target="_blank" href="https://youtube-clone-v3-using-react-aqky.vercel.app/"> <img src={youtube} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+         <a target="_blank" href="https://tranquil-fairy-e92041.netlify.app/"> <img src={socail} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a target="_blank" href="https://movie-app-using-react-tau.vercel.app/"> <img src={movie} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a target="_blank" href="  https://food-order-react-three.vercel.app/"> <img src={food} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a  target="_blank" href=" https://expenses-with-chart-using-react.vercel.app/"> <img style={{height:"50%"}} src={chart} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a  target="_blank" href="https://bootstrap-upwork-clone.vercel.app/"> <img style={{height:"50%"}} src={upwork} alt="" /></a>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+    {/*////////////////////////////////////////////////////////////////////////////// backend */}
+    <div className="portfolio" id="portfolio">
+      {/* heading */}
+      <span  style={{color: darkMode?'white': '',display:"none"}}>Recent Projects</span>
       <span>Portfolio</span>
 
       {/* slider */}
@@ -24,19 +61,26 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <a target="_blank" href="https://youtube-clone-v3-using-react-aqky.vercel.app/"> <img src={youtube} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+         <a target="_blank" href="https://tranquil-fairy-e92041.netlify.app/"> <img src={socail} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <a target="_blank" href="https://movie-app-using-react-tau.vercel.app/"> <img src={movie} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt="" />
+          <a target="_blank" href="  https://food-order-react-three.vercel.app/"> <img src={food} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a  target="_blank" href=" https://expenses-with-chart-using-react.vercel.app/"> <img style={{height:"50%"}} src={chart} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a  target="_blank" href="https://bootstrap-upwork-clone.vercel.app/"> <img style={{height:"50%"}} src={upwork} alt="" /></a>
         </SwiperSlide>
       </Swiper>
     </div>
+    </>
   );
 };
 
